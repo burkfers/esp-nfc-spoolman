dfe8f11b069fec88baab6f8de520c1c1e2663204
burkfers <6268175+burkfers@users.noreply.github.com>
2025-06-10 21:02:40 +0200

    led test

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ MOONRAKER_HOST = '192.168.0.41'
 MOONRAKER_PORT = 80
 CS_GPIO = 16  # Chip Select GPIO for PN532
 DUMMY = False  # Set to True to use dummy NFC reader for testing
+
+LED_GPIO = 5  # GPIO for LED indicator
